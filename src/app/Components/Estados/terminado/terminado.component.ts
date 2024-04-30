316ff3a90a2cf413b3921c6278a25f3792a50d3f
Josue Aguilar <josueaguilarjnai@gmail.com>
2024-04-30 08:50:03 -0600

    correcciones finales, version funcional

--- src/app/Components/Estados/terminado/terminado.component.ts
+++ src/app/Components/Estados/terminado/terminado.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
 import {GeneralService} from "../../../Services/peticiones/general.service";
-import {sumTimer, timeToString} from "../../../utils/TimeOperations";
+import {restTime, sumTimer, timeToString} from "../../../utils/TimeOperations";
 import {ProcessTime} from "../../../Classes/ProcessTime";
+import {Proceso} from "../../../Classes/Peticiones/Proceso";
 
 @Component({
   selector: 'app-terminado',
@@ -26,4 +27,12 @@ export class TerminadoComponent {
     let result = new ProcessTime();
     this.generalService.processResult.procesoTerminado.forEach((value, index) => result = sumTimer(result, value.tiempoRetorno))
     return timeToString(result);  }
+
+  calculateTiempoRespuesta(process: Proceso) {
+    if (process.respuesta){
+      const respuesta = restTime(process.tiempoRespuesta, process.tiempoLlegada)
+      return timeToString(respuesta)
+    }
+    return timeToString(new ProcessTime())
+  }
 }
